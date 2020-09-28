16bd8926bddd78a6a158514e660486a2e2a5d2da
3k-Grubabby <wc549476585@outlook.com>
2020-09-28 16:28:04 +0800

    0928

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -7,121 +7,148 @@ import (
 
 func init() {
 
-	beego.GlobalControllerRouter["youku/controllers:BaseController"] = append(beego.GlobalControllerRouter["youku/controllers:BaseController"],
-		beego.ControllerComments{
-			Method:           "ChannelRegion",
-			Router:           "/channel/region",
-			AllowHTTPMethods: []string{"*"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:BarrageController"] = append(beego.GlobalControllerRouter["youku/controllers:BarrageController"],
+        beego.ControllerComments{
+            Method: "Save",
+            Router: "/barrage/save",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["youku/controllers:BaseController"] = append(beego.GlobalControllerRouter["youku/controllers:BaseController"],
-		beego.ControllerComments{
-			Method:           "ChannelType",
-			Router:           "/channel/type",
-			AllowHTTPMethods: []string{"*"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:BarrageController"] = append(beego.GlobalControllerRouter["youku/controllers:BarrageController"],
+        beego.ControllerComments{
+            Method: "BarrageWs",
+            Router: "/barrage/ws",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["youku/controllers:CommentController"] = append(beego.GlobalControllerRouter["youku/controllers:CommentController"],
-		beego.ControllerComments{
-			Method:           "List",
-			Router:           "/comment/list",
-			AllowHTTPMethods: []string{"*"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:BaseController"] = append(beego.GlobalControllerRouter["youku/controllers:BaseController"],
+        beego.ControllerComments{
+            Method: "ChannelRegion",
+            Router: "/channel/region",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["youku/controllers:CommentController"] = append(beego.GlobalControllerRouter["youku/controllers:CommentController"],
-		beego.ControllerComments{
-			Method:           "Save",
-			Router:           "/comment/save",
-			AllowHTTPMethods: []string{"*"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:BaseController"] = append(beego.GlobalControllerRouter["youku/controllers:BaseController"],
+        beego.ControllerComments{
+            Method: "ChannelType",
+            Router: "/channel/type",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["youku/controllers:UserControler"] = append(beego.GlobalControllerRouter["youku/controllers:UserControler"],
-		beego.ControllerComments{
-			Method:           "LoginDo",
-			Router:           "/login/do",
-			AllowHTTPMethods: []string{"*"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:CommentController"] = append(beego.GlobalControllerRouter["youku/controllers:CommentController"],
+        beego.ControllerComments{
+            Method: "List",
+            Router: "/comment/list",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["youku/controllers:UserControler"] = append(beego.GlobalControllerRouter["youku/controllers:UserControler"],
-		beego.ControllerComments{
-			Method:           "SaveRegister",
-			Router:           "/register/save",
-			AllowHTTPMethods: []string{"post"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:CommentController"] = append(beego.GlobalControllerRouter["youku/controllers:CommentController"],
+        beego.ControllerComments{
+            Method: "Save",
+            Router: "/comment/save",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
-		beego.ControllerComments{
-			Method:           "ChannelAdvert",
-			Router:           "/channel/advert",
-			AllowHTTPMethods: []string{"*"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:UserControler"] = append(beego.GlobalControllerRouter["youku/controllers:UserControler"],
+        beego.ControllerComments{
+            Method: "LoginDo",
+            Router: "/login/do",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
-		beego.ControllerComments{
-			Method:           "ChannelHotList",
-			Router:           "/channel/hot",
-			AllowHTTPMethods: []string{"*"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:UserControler"] = append(beego.GlobalControllerRouter["youku/controllers:UserControler"],
+        beego.ControllerComments{
+            Method: "SaveRegister",
+            Router: "/register/save",
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
-		beego.ControllerComments{
-			Method:           "ChannelRecommendRegionList",
-			Router:           "/channel/recommend/region",
-			AllowHTTPMethods: []string{"*"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:UserControler"] = append(beego.GlobalControllerRouter["youku/controllers:UserControler"],
+        beego.ControllerComments{
+            Method: "SendMessageDo",
+            Router: "/send/message",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
-		beego.ControllerComments{
-			Method:           "GetChannelRecomendTypeList",
-			Router:           "/channel/recommend/type",
-			AllowHTTPMethods: []string{"*"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
+        beego.ControllerComments{
+            Method: "ChannelAdvert",
+            Router: "/channel/advert",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
-		beego.ControllerComments{
-			Method:           "ChannelVideo",
-			Router:           "/channel/video",
-			AllowHTTPMethods: []string{"*"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
+        beego.ControllerComments{
+            Method: "ChannelHotList",
+            Router: "/channel/hot",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
-		beego.ControllerComments{
-			Method:           "VideoEpisodesList",
-			Router:           "/video/episodes/list",
-			AllowHTTPMethods: []string{"*"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
+        beego.ControllerComments{
+            Method: "ChannelRecommendRegionList",
+            Router: "/channel/recommend/region",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
-		beego.ControllerComments{
-			Method:           "VideoInfo",
-			Router:           "/video/info",
-			AllowHTTPMethods: []string{"*"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
+        beego.ControllerComments{
+            Method: "GetChannelRecomendTypeList",
+            Router: "/channel/recommend/type",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
+        beego.ControllerComments{
+            Method: "ChannelVideo",
+            Router: "/channel/video",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
+        beego.ControllerComments{
+            Method: "VideoEpisodesList",
+            Router: "/video/episodes/list",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["youku/controllers:VideoController"] = append(beego.GlobalControllerRouter["youku/controllers:VideoController"],
+        beego.ControllerComments{
+            Method: "VideoInfo",
+            Router: "/video/info",
+            AllowHTTPMethods: []string{"*"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
 }
